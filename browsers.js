30365e924ecb46a193151559c5d7c3d42c4e2be2
Andrea <andrea.verlicchi@ynap.com>
2017-09-14 14:35:57 +0200

    Unsupporting all modern browser

--- browsers.js
+++ browsers.js
@@ -1,10 +1,10 @@
 export default {
     IE9: true,
     IE10: true,
-    IE11: true,
-    Edge: true,
-    Chrome: true,
-    Firefox: true,
-    Safari: true,
-    Opera: true
+    IE11: false,
+    Edge: false,
+    Chrome: false,
+    Firefox: false,
+    Safari: false,
+    Opera: false
 }
